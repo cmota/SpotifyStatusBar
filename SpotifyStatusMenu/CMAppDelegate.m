9343d04eed73810ac413ac4d9049847516417435
cmota <cafonsomota@gmail.com>
2013-11-25 21:38:26 +0000

    Initial Commit

---
+++ SpotifyStatusMenu/CMAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  CMAppDelegate.m
+//  SpotifyStatusMenu
+//
+//  Created by Carlos Mota on 11/25/13.
+//  Copyright (c) 2013 Carlos Mota. All rights reserved.
+//
+
+#import "CMAppDelegate.h"
+
+@implementation CMAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ SpotifyStatusMenu/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  SpotifyStatusMenu
+//
+//  Created by Carlos Mota on 11/25/13.
+//  Copyright (c) 2013 Carlos Mota. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
